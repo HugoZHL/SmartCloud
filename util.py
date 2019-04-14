751aa64fcf225927413270f88b346b42d807c644
wurundi <rundi_wu@pku.edu.cn>
2019-04-14 15:39:30 +0800

    Update util functions, test needed

--- util.py
+++ util.py
@@ -1,9 +1,67 @@
 # for functions
+import os
+import json
+import time
+import shutil
+
+# System root directory
+# make sure file operations needs no 'sudo' under this directory
+# keep last character be '/'
+ROOT_DIR = '/home/admin/'    # FIXME : change this if necessary
+UCONFIG_PATH = os.path.join(ROOT_DIR, 'uconfig.json')
 
 
-def valid_login(account: str, password: str) -> 'str':
+####################################################################
+#                       user login and register                    #
+####################################################################
+
+def valid_login(account: str, password: str) -> 'str' or None:
+    """Validate login
+
+    :param account: str. Account username.
+    :param password: str. Account password.
+    :return: None or str. None if valid else error message.
+    """
+    uconfig = json.load(UCONFIG_PATH)   # uconfig are username-hash(password) pairs
+
+    # account name not found
+    if account not in uconfig.keys():
+        return 'Username or password not correct.'
+
+    # password not correct
+    if uconfig[account] != hash(password):
+        return 'Username or password not correct.'
+
     return None
 
+
+def register(account: str, password: str) -> 'str' or None:
+    """Register a new account. After registration, a home directory
+    will be created for this user.
+
+    :param account: str. Account username.
+    :param password: str. Account password.
+    :return: None or str. None if valid else error message.
+    """
+    uconfig = json.load(UCONFIG_PATH)   # uconfig are username-hash(password) pairs
+
+    # if account name conflict
+    if account in uconfig.keys():
+        return "Username has already been used."
+
+    # set username-hash(password) pairs
+    uconfig[account] = hash(password)
+    with open(UCONFIG_PATH, 'w') as f:
+        json.dump(uconfig, f)
+
+    # create home directory 'ROOT_DIR/account'
+    os.mkdir(os.path.join(ROOT_DIR, account))
+    return None
+
+
+####################################################################
+#                       main file operations                       #
+####################################################################
 
 # dict.keys() = ['name', 'size', 'time', 'type', 'folder']
 # value都是str
@@ -11,8 +69,13 @@ def valid_login(account: str, password: str) -> 'str':
 # time为最后修改时间，'2019/04/13 14:07'
 # type为文件类型，'dir'表示文件夹，其他的无所谓，但会显示
 # folder为所属文件夹，结尾一定是'/'
-def get_files(account: str, path: str) -> 'list[dict{}]':
-    files = [
+def get_files(account: str, path: str) -> 'list':
+    """Get information of all files under given path.
+
+    :param account: str. Account username.
+    :param path: str. File path (user-view).
+    :return: list. A list of file information.
+        files = [
         {
             'name': 'test',
             'size': '8.93 KB',
@@ -27,40 +90,159 @@ def get_files(account: str, path: str) -> 'list[dict{}]':
             'type': 'py',
             'folder': '/'
         }
-    ]
+        ]
+    """
+    user_root = userpath2abspath(account, '/')
+    filenames = sorted(os.listdir(path))
+
+    files = []
+    for name in filenames:
+        fpath = os.path.join(user_root, path, name)
+        info = get_file_info(fpath)
+        files.append(info)
+
     return files
 
 
 # 返回类型同上，用于搜索包含search的文件
-def search_files(account: str, search: str) -> 'list[dict{}]':
-    files = [
-        {
-            'name': 'test',
-            'size': '8.93 KB',
-            'time': '2019/04/13 14:07',
-            'type': 'dir',
-            'folder': '/'
-        },
-        {
-            'name': 'test.py',
-            'size': '149 B',
-            'time': '2018/02/03 07:03',
-            'type': 'py',
-            'folder': '/'
-        }
-    ]
+def search_files(account: str, search: str) -> 'list':
+    """Search for matching files given a search string.
+
+    :param account: str. Account username.
+    :param search: str. Search string.
+    :return: list. A list of file information. If not found, empty list.
+    """
+    user_root = userpath2abspath(account, '/')
+
+    files = []
+    for root, dirs, files in os.walk(user_root):
+        for name in dirs + files:
+            if search in name:
+                info = get_file_info(os.path.join(root, name))
+                files.append(info)
     return files
 
 
-def make_new_folder(account: str, filepath: str, new_folder: str) -> 'None':
-    pass
+def make_new_folder(account: str, filepath: str, new_folder: str) -> None or str:
+    """ Make a new folder.
+
+    :param account: str. Account username.
+    :param filepath: str. Current file path (user-view).
+    :param new_folder: str. New folder name.
+    :return: None or str. None if succeed else error message.
+    """
+    abspath = userpath2abspath(account, filepath)
+    new_path = os.path.join(abspath, new_folder)
+    if os.path.exists(new_path):
+        return 'Folder with the same name already exists.'
+
+    os.mkdir(new_path)
+    return None
 
 
 # 将filepath的文件（夹）名字改成target
-def rename_at_server(account: str, filepath: str, target: str) -> 'None':
-    pass
+def rename_at_server(account: str, filepath: str, target: str) -> None or str:
+    """ Rename a file or dir.
+
+    :param account: str. Account username.
+    :param filepath: str. Source file path (user-view).
+    :param target: str. New name.
+    :return: None or str. None if succeed else error message.
+    """
+    abspath = userpath2abspath(account, filepath)
+
+    # check conflict
+    pardir = os.path.dirname(abspath)
+    filenames = os.listdir(pardir)
+    if target in filenames:
+        return 'File with the same name already exists in this directory.'
+
+    # check validity
+    if '/' in target:
+        return "Filename cannot contain '/'."
+
+    new_path = os.path.join(pardir, target)
+    os.rename(abspath, new_path)
+    return None
 
 
-def delete_from_server(account: str, filepath: str) -> 'None':
-    # do some deletion
-    pass
+def delete_from_server(account: str, filepath: str) -> None or str:
+    """ Delete a file or dir.
+
+    :param account: str. Account username.
+    :param filepath: str. Target file path (user-view).
+    :return: None or str. None if succeed else error message.
+    """
+    abspath = userpath2abspath(account, filepath)
+
+    if not os.path.exists(abspath):
+        return 'Target file not exists.'
+
+    if not os.path.isdir(abspath):
+        os.remove(abspath)
+    else:
+        shutil.rmtree(abspath)
+    return None
+
+
+####################################################################
+#                          help functions                          #
+####################################################################
+
+def abspath2userpath(fpath: str) -> str:
+    """converting absolute path to user-view path
+    'ROOT_DIR/username/xxx' ---> '/xxx'
+    """
+    return '/' + fpath[len(ROOT_DIR):]
+
+
+def userpath2abspath(account: str, fpath: str) -> str:
+    """converting user-view path to absolute path
+    '/xxx' ---> 'ROOT_DIR/username/xxx'
+    """
+    return ROOT_DIR + account + fpath
+
+
+def get_dir_size(fdir: str) -> str:
+    """get directory true size (summing up all files under it)"""
+    size = 0
+    for root, dirs, files in os.walk(fdir):
+        size += sum([os.path.getsize(os.path.join(root, name)) for name in files])
+    return size
+
+
+def timestamp2time(timestamp: float) -> str:
+    """convert time stamp to formatted time"""
+    timeStruct = time.localtime(timestamp)
+    return time.strftime('%Y-%m-%d %H:%M:%S', timeStruct)
+
+
+def get_file_info(fpath: str) -> dict:
+    """Get formatted file information"""
+    # get file name
+    name = fpath.split('/')[-1]
+
+    # get file size
+    fsize = os.path.getsize(fpath) # directory size will always be 4K
+
+    # get file modify time
+    ftime = timestamp2time(os.path.getmtime(fpath))
+
+    # get file type
+    if os.path.isdir(fpath):
+        ftype = 'dir'
+    else:
+
+        ftype = name.split('.')[-1] if '.' in name else ''
+
+    # get file folder (user-view)
+    ffolder = abspath2userpath(os.path.dirname(fpath))
+
+    info = {
+        'name': name,
+        'size': fsize,
+        'time': ftime,
+        'type': ftype,
+        'folder': ffolder
+    }
+    return info
